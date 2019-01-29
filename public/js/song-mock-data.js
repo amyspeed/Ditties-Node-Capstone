40587a6bff141dcfa09c10d6e7b99d6f6c4205f9
Amy <amy.speed.henley@gmail.com>
2019-01-29 03:15:18 -0600

    moved Mock-data

---
+++ public/js/song-mock-data.js
@@ -0,0 +1,89 @@
+//Song mock data
+
+let MOCK_SONGS = {
+    "songs": [
+        {
+            "id": "111",
+            "title": "Buggy Software Blues",
+            "coauthors": "Person Peoples",
+            "genreFeel": "digital blues",
+            "speed": "slow",
+            "key": "G",
+            "capo": 2,
+            "timeSig": {
+                "top": 6,
+                "bottom": 8
+            },
+            "strum": "D UDUU",
+            "notes": "Harmonica on lead",
+            "content": [
+                {
+                    "sectionId": 1,
+                    "section": "intro",
+                    "chords": "G7 C7 G7",
+                    "lyrics": ""
+                },
+                {
+                    "sectionId": 2,    
+                    "section": "verse",
+                    "chords": "G7 C7 G7 G7 C7 C7 G7 G7 D7 C7 G7 D7",
+                    "lyrics": "Buggy Software Blues! I got Buggy Software Blues"
+                }
+            ]
+        },
+
+        {
+            "id": "222",
+            "title": "Spicy JavaScript",
+            "coauthors": "",
+            "genreFeel": "Techno pop with a latin feel",
+            "speed": "fast",
+            "key": "C",
+            "capo": 4,
+            "timeSig": {
+                "top": 4,
+                "bottom": 4
+            },
+            "strum": "D D DUXU",
+            "notes": "Ellie Goulding meets Selina",
+            "content": [
+                {   
+                    "sectionId": 1,
+                    "section": "intro",
+                    "chords": "C Am G D",
+                    "lyrics": ""
+                },
+                {
+                    "sectionId": 2,
+                    "section": "verse",
+                    "chords": "C Am G D",
+                    "lyrics": "Dancing, magic, love, butterflies, jalepenos, javaScript"
+                },
+                {   
+                    "sectionId": 3,
+                    "section": "verse",
+                    "chords": "C Am G D",
+                    "lyrics": "Blah blah blah blah"
+                },
+                {
+                    "sectionId": 4,
+                    "section": "chorus",
+                    "chords": "C C G C",
+                    "lyrics": "JavaScript, spicy, blahhh"
+                },
+                {
+                    "sectionId": 5,
+                    "section": "verse",
+                    "chords": "C Am G D",
+                    "lyrics": "Pop stuff...."
+                },
+                {
+                    "sectionId": 6,
+                    "section": "bridge",
+                    "chords": "Em Em Em Em",
+                    "lyrics": ""
+                }
+            ]
+        }
+    ]
+};
