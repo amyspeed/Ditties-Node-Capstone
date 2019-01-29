3fffe8863a5cda511f05f849522ced13537f109a
Amy <amy.speed.henley@gmail.com>
2019-01-28 23:28:36 -0600

    initial branch commit

--- public/js/songs.js
+++ public/js/songs.js
@@ -96,8 +96,22 @@ function getSongs(callbackFn) {
 function displaySongs(data) {
     for (index in data.songs) {
         $('body').append(
-            `<p>${data.songs[index].title}</p>`
-        );
+            `<p>Title: ${data.songs[index].title}<br>
+            Coauthor(s): ${data.songs[index].coauthors}<br>
+            Genre/Feel: ${data.songs[index].genreFeel}<br>
+            Speed: ${data.songs[index].speed}<br>
+            Key: ${data.songs[index].key} Capo: ${data.songs[index].capo}<br>
+            Time Signature: ${data.songs[index].timeSig.top}/${data.songs[index].timeSig.bottom}<br>
+            Strum Notes: ${data.songs[index].strum}<br>
+            Other Misc. Notes: ${data.songs[index].notes}<br>
+            ${data.songs[index].content[0].section}<br>
+            Chords: ${data.songs[index].content[0].chords}<br>
+            Lyrics: ${data.songs[index].content[0].lyrics}<br>
+            ${data.songs[index].content[1].section}<br>
+            Chords: ${data.songs[index].content[1].chords}<br>
+            Lyrics: ${data.songs[index].content[1].lyrics}<br></p>`
+            );
+        
     }
 }
 
