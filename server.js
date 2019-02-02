1e2a180aefec7896587b6051606736602e994599
Amy <amy.speed.henley@gmail.com>
2019-02-02 00:47:21 -0600

    Passport dependency

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
 mongoose.Promise = global.Promise;
 
 const { DATABASE_URL, PORT } = require('./config');
